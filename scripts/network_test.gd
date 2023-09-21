fb20e3d0b2fa0c3bf44c9972d9cd6c98c30388d7
raphael lesbros <raphael.lesbros@gmail.com>
2023-09-21 17:16:07 +0200

    Upgraded network test interface a bit

--- scripts/network_test.gd
+++ scripts/network_test.gd
@@ -3,15 +3,27 @@ extends Control
 func _ready() -> void:
 	NetworkManager.send_message.connect(_display_in_console)
 	ActionManager.send_message.connect(_display_in_console)
+	NetworkManager.connection_done.connect(_start_game)
 
 func _on_join_button_pressed() -> void:
 	NetworkManager.create_client("127.0.0.1", "8888")
+	_disable_buttons()
 
 func _on_host_button_pressed() -> void:
 	NetworkManager.create_server("8888")
+	_disable_buttons()
 
 func _on_start_game_button_pressed() -> void:
+	_start_game()
+
+func _start_game() -> void:
 	ActionManager.start_game()
+	_disable_buttons()
+
+func _disable_buttons() :
+	($HostButton as Button).disabled = true
+	($JoinButton as Button).disabled = true
+	($StartGameButton as Button).disabled = true
 
 func _display_in_console(message : String) -> void :
 	($Console as RichTextLabel).text += message + "\n"
