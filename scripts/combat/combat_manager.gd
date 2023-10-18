5c1f105970222eb99e42820e0a0d3771b28154cc
raphael lesbros <raphael.lesbros@gmail.com>
2023-10-18 11:02:24 +0200

    Cleaned up and added comments to combat_manager

--- scripts/combat/combat_manager.gd
+++ scripts/combat/combat_manager.gd
@@ -1,22 +1,25 @@
 extends Node
 class_name CombatManager
 
-enum Turn { NONE, HOST, CLIENT }
+enum Turn { HOST, CLIENT }
 var _current_turn : Turn :
 	set (value) :
 		_current_turn = value
-		if _current_turn != Turn.NONE :
-			_start_turn()
+		_start_turn()
 
 @onready var events : CombatEventManager = $CombatEventManager
 
+## Stores pending effects, in queue order. Effects will resolve as soon as the previous one is done resolving.
 var effect_queue : Array[Effect]
 var current_effect : Effect
+## Stores pending actions, in queue order. Actions when resolve as soon as no more effects are pending.
+## This prevents actions from causing desync when called while an other action resolves.
 var action_queue : Array[Action]
 
 signal effect_queue_emptied()
 signal action_queue_emptied()
 
+## Initializes all combat relevant data.
 @rpc("authority", "call_local", "reliable")
 func start_game() :
 	GameManager.player = Player.new(NetworkManager.is_host())
@@ -27,6 +30,7 @@ func start_game() :
 	GameManager.get_player(false).refill_hand()
 	_set_random_game_turn()
 
+## Try to play a card. If network is used, the client sends this request to the host.
 @rpc("any_peer", "call_remote", "reliable")
 func query_card_play(card_is_host : bool, card_index : int, target_is_host : bool, target_index : int) -> void :
 	if !GameManager.get_player(card_is_host).can_play_card(card_index) : return
@@ -35,20 +39,23 @@ func query_card_play(card_is_host : bool, card_index : int, target_is_host : boo
 	else :
 		query_card_play.rpc(card_is_host, card_index, target_is_host, target_index)
 
+## Try to end the turn. If network is used, the client sends this request to the host.
 @rpc("any_peer", "call_remote", "reliable")
 func query_end_turn() -> void :
 	if NetworkManager.is_host() :
-		if !turn_is_none() and ((multiplayer.get_remote_sender_id() == 0) == is_player_turn()) :
+		if ((multiplayer.get_remote_sender_id() == 0) == is_player_turn()) :
 			_apply_end_turn.rpc()
 		else :
 			push_error("Cannot end turn while not current turn")
 	else :
 		query_end_turn.rpc()
 
+## Plays a card. This function should ony be called by the host, who sends it to the client.
 @rpc("authority", "call_local", "reliable")
 func _apply_card_play(card_is_host : bool, card_index : int, target_is_host : bool, target_index : int) -> void :
 	_add_action(PlayCardAction.new(card_is_host, card_index, target_is_host, target_index))
 
+## Ends the trun. This function should only be called by the host, who sends it to the client.
 @rpc("authority", "call_local", "reliable")
 func _apply_end_turn() -> void :
 	_add_action(EndTurnAction.new())
@@ -56,26 +63,22 @@ func _apply_end_turn() -> void :
 func _set_random_game_turn() -> void :
 	_current_turn = Turn.CLIENT if NetworkManager.is_multiplayer and GameManager.rng.randi_range(0, 1) else Turn.HOST
 
+## Goes to next turn.
+## In single player mode, automatically skip the opponent's turn.
 func next_turn() -> void :
-	if _current_turn == Turn.NONE : 
-		push_error("CANNOT DO TO NEXT TURN IF CURRENT TURN IS NONE")
-		return
-	elif _current_turn == Turn.HOST :
+	if _current_turn == Turn.HOST :
 		_current_turn = Turn.CLIENT
 		if !NetworkManager.is_multiplayer : 
 			next_turn()
-	elif _current_turn == Turn.CLIENT : 
+	else :
 		_current_turn = Turn.HOST
 
 func _start_turn() -> void :
 	get_player_by_turn(true).start_turn()
 	events.on_turn_start(_current_turn)
 
-func turn_is_none() -> bool :
-	return _current_turn == Turn.NONE
-
 func is_player_turn() -> bool :
-	return !turn_is_none() and ((_current_turn == Turn.HOST) == GameManager.player.is_host)
+	return ((_current_turn == Turn.HOST) == GameManager.player.is_host)
 
 func get_player_by_turn(active_player : bool) -> Player :
 	if active_player == is_player_turn() :
@@ -83,6 +86,8 @@ func get_player_by_turn(active_player : bool) -> Player :
 	else :
 		return GameManager.opponent
 
+## Adds an effect to the queue, to be resolved after all previous effects did so.
+## If no effect is currently resolving, starts resolving effects in queue order.
 func add_effect(effect : Effect, source : Character, target : Character) -> void :
 	effect.source = source
 	effect.target = target
@@ -90,50 +95,57 @@ func add_effect(effect : Effect, source : Character, target : Character) -> void
 	if current_effect == null :
 		_apply_effect()
 
+## Adds multiple effects to the queue, to be resolved after all previous effects did so.
+## If no effect is currently resolving, starts resolving effects in queue order.
 func add_effects(effects : Array[Effect], source : Character, target : Character) -> void :
 	for index in range(0, effects.size()) :
 		add_effect(effects[index], source, target)
 
+## Adds an effect in front of the queue, to be resolved before other effects.
+## Should only be called by "wrapper" effects, and will lock the game if used while no effect is active.
+## To call this multiple times, use add_effects_immediate or add the effects in reverse execution order.
 func add_effect_immediate(effect : Effect, source : Character, target : Character) -> void :
 	effect.source = source
 	effect.target = target
 	effect_queue.push_front(effect)
-	if current_effect == null :
-		_apply_effect()
 
+## Adds multiple effects in front of the queue, to be resolved before other effects.
+## Should only be called by "wrapper" effects, and will lock the game if used while no effect is active.
 func add_effects_immediate(effects : Array[Effect], source : Character, target : Character) -> void :
 	for index in range(effects.size()-1, -1, -1) :
 		add_effect_immediate(effects[index], source, target)
 
+## Applies the first pending effect, and the following ones if any.
+## Emits effect_queue_emptied when no more effects are pending.
 func _apply_effect() -> void :
-	current_effect = effect_queue.pop_front()
-	current_effect.apply()
-	if ! current_effect.is_done :
-		await current_effect.done
+	while effect_queue.size() > 0 :
+		current_effect = effect_queue.pop_front()
+		current_effect.apply()
+		if ! current_effect.is_done :
+			await current_effect.done
 	current_effect = null
-	if effect_queue.size() > 0 :
-		_apply_effect()
-	else :
-		effect_queue_emptied.emit()
+	effect_queue_emptied.emit()
 
+## Adds an action to the queue, and applies it if action is currently active.
 func _add_action(action : Action) -> void :
 	action_queue.append(action)
 	if action_queue.size() == 1 :
 		_apply_action()
 
+## Applies the first pending action, and the following ones if any.
+## Checks game state after each action, and emits action_queue_emptied when no more actions are pending.
 func _apply_action() -> void :
-	action_queue[0].apply()
-	if current_effect != null or effect_queue.size() != 0 :
-		await effect_queue_emptied
-	_check_game_state()
-	action_queue.remove_at(0)
-	if action_queue.size() > 0:
-		_apply_action()
-	else :
-		action_queue_emptied.emit()
+	while action_queue.size() > 0 :
+		action_queue[0].apply()
+		if current_effect != null or effect_queue.size() != 0 :
+			await effect_queue_emptied
+		_check_game_state()
+		action_queue.remove_at(0)
+	action_queue_emptied.emit()
 
+## Updates characters is_dead value, then checks if a player wins the game.
 func _check_game_state() -> void :
-	_update_character_states()	
+	_update_character_states()
 	while (current_effect != null or effect_queue.size() != 0) :
 		await effect_queue_emptied
 		_update_character_states()
