73af85f05f732c7ec68bc762386f458f1303ccb2
raphael lesbros <raphael.lesbros@gmail.com>
2023-10-24 10:57:27 +0200

    Removed some Resource logic from statuses (temporary)

--- scripts/combat/status_effects/status_effect.gd
+++ scripts/combat/status_effects/status_effect.gd
@@ -1,16 +1,23 @@
 extends Resource
 class_name StatusEffect
 
-@export var id : String
-@export var decay_on_turn_start : bool
-@export var expire_at_zero_stacks : bool
+var id : String
+var decay_on_turn_start : bool
+var expire_at_zero_stacks : bool
+
 @export var stacks : int :
 	set(value) :
 		stacks = value
 		if stacks == 0 and expire_at_zero_stacks :
 			owner.remove_status(id)
 
+
 var owner : Character
+
+func _init() -> void :
+	id = "default_status"
+	decay_on_turn_start = false
+	expire_at_zero_stacks = false
 	
 func decay() -> void :
 	if decay_on_turn_start :
