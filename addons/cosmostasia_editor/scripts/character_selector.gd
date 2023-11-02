876730ee582d08bfc64aa4a67b97435848e669df
raphael lesbros <raphael.lesbros@gmail.com>
2023-11-02 14:59:39 +0100

    Improved character selection

--- addons/cosmostasia_editor/scripts/character_selector.gd
+++ addons/cosmostasia_editor/scripts/character_selector.gd
@@ -1,9 +1,8 @@
 @tool
-extends MenuBar
+extends OptionButton
 
 @export_dir var _character_folder : String
 var _file_names : Array[String]
-@onready var _popup_menu : PopupMenu = $PopupMenu
 
 var character : CharacterInfo
 
@@ -11,14 +10,15 @@ signal character_selected()
 
 func _ready() -> void:
 	_refresh_options()
+	select(-1)
 
 func _refresh_options() :
-	_popup_menu.clear()
+	clear()
 	var dir : DirAccess = DirAccess.open(_character_folder+"/")
 	_file_names.append_array(dir.get_files())
 	for file in _file_names :
-		_popup_menu.add_item(file.replace(".tres", "").to_pascal_case())
+		add_item(file.replace(".tres", "").to_pascal_case())
 
-func _on_popup_menu_id_pressed(id: int) -> void:
-	character = load(_character_folder + "/" + _file_names[id])
+func _on_item_selected(index: int) -> void:
+	character = load(_character_folder + "/" + _file_names[index])
 	character_selected.emit()
