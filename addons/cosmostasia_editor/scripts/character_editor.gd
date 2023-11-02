ae130fb218e6b2078468e6e28b4e04b4f3a72a7f
raphael lesbros <raphael.lesbros@gmail.com>
2023-11-02 17:19:24 +0100

    Added character creation to character_editor

--- addons/cosmostasia_editor/scripts/character_editor.gd
+++ addons/cosmostasia_editor/scripts/character_editor.gd
@@ -1,16 +1,32 @@
 @tool
 extends Control
 
-@onready var _character_selector = %CharacterSelector
+var _character : CharacterInfo :
+	set(value) :
+		_character = value
+		_update_fields()
 
-func _on_character_selected() -> void:
-	for field in %EditContainer.find_children("", "DataField", true) :
-		field.set_value(_character_selector.character.get(field.property_name))
+func _ready() -> void:
+	_update_fields()
+
+func _on_character_selected(character : CharacterInfo) -> void:
+	_character = character
+
+func _update_fields() -> void :
+	($EditPanel as Control).visible = _character != null
+	if _character != null :
+		for field in %EditContainer.find_children("", "DataField", true) :
+			field.set_value(_character.get(field.property_name))
 
 func _on_save_button_pressed() -> void:
-	save()
-
-func save() -> void :
-	if _character_selector.character == null : return
+	if _character == null : return
 	for field in %EditContainer.find_children("", "DataField", true) :
-		_character_selector.character.set(field.property_name, field.get_value())
+		_character.set(field.property_name, field.get_value())
+
+func _on_create_button_pressed() -> void :
+	(%CreatePathSelector as FileDialog).popup()
+
+func _on_create_path_selected(path : String) -> void :
+	_character = CharacterInfo.new()
+	ResourceSaver.save(_character, path)
+	%CharacterSelector.select_file(path.get_slice("/", path.get_slice_count("/") - 1))
--- addons/cosmostasia_editor/scripts/character_selector.gd
+++ addons/cosmostasia_editor/scripts/character_selector.gd
@@ -4,15 +4,14 @@ extends OptionButton
 @export_dir var _character_folder : String
 var _file_names : Array[String]
 
-var character : CharacterInfo
-
-signal character_selected()
+signal character_selected(character : CharacterInfo)
 
 func _ready() -> void:
 	_refresh_options()
 	select(-1)
 
 func _refresh_options() :
+	_file_names = []
 	clear()
 	var dir : DirAccess = DirAccess.open(_character_folder+"/")
 	_file_names.append_array(dir.get_files())
@@ -20,5 +19,11 @@ func _refresh_options() :
 		add_item(file.replace(".tres", "").to_pascal_case())
 
 func _on_item_selected(index: int) -> void:
-	character = load(_character_folder + "/" + _file_names[index])
-	character_selected.emit()
+	character_selected.emit(load(_character_folder + "/" + _file_names[index]))
+
+func select_file(file_name : String) -> void :
+	_refresh_options()
+	for i in range(_file_names.size()) :
+		print(_file_names[i] + " / " + file_name)
+		if _file_names[i] == file_name :
+			select(i)
