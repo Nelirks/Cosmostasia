27092f7f7039bc8faf23a3b17b0df2c5e2ba6d37
raphael lesbros <raphael.lesbros@gmail.com>
2023-11-12 14:01:11 +0100

    Removed GameConditions and ConditionalEffects
    Conditions are now check on effect emission rather than resolution

--- scripts/combat/game_condition.gd
+++
@@ -1,5 +0,0 @@
-extends RefCounted
-class_name GameCondition
-
-func is_met() -> bool :
-	return false
--- scripts/data/cards/frappe_energisee.gd
+++ scripts/data/cards/frappe_energisee.gd
@@ -5,4 +5,5 @@ extends Card
 
 func apply_effects(target : Character) -> void :
 	_add_effect(DamageEffect.new(damage, character, target))
-	_add_effect(ConditionalEffect.new(EnergyRemainingCondition.new(character.player, 1), [ApplyArmorEffect.new(armor, character, character)]))
+	if character.player.current_energy > 0 :
+		_add_effect(ApplyArmorEffect.new(armor, character, character))
