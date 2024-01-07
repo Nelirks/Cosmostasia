e10a36d39539ce5eba7816b95c8cdece1c7569d9
Simon Gosse <simongosse.etude@gmail.com>
2024-01-07 19:09:10 +0100

    Spell cards instatiate with the rights elements

--- scripts/UI/cards_ui.gd
+++ scripts/UI/cards_ui.gd
@@ -5,6 +5,7 @@ var card: Card:
 	set(value):
 		card = value
 		on_position_set()
+		update_card_infos()
 var miracle_position: Vector2
 var middle_position: Vector2
 var prepared_position: Vector2
@@ -21,3 +22,18 @@ func on_position_set():
 			position = miracle_position
 		Card.Position.DISCARD_PILE:
 			visible = false
+
+func update_card_infos():
+	(%Card_Title as Label).text = card.card_name
+	(%Card_Cost as Label).text = str(card.cost)
+	(%Card_Character as Label).text = str(card.character)
+	(%CardDescription as RichTextLabel).text = card.description
+	(%CardFlavorText as RichTextLabel).text = card.quote
+
+
+func _on_card_background_mouse_entered():
+	%Card_Background.set_position(0,100)
+
+
+func _on_card_background_mouse_exited():
+	%Card_Background.set_position(0,0)
