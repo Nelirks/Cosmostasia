6222b734369e586b32c9c9078d1711fdc3a67a95
Nelirks <raphael.lesbros@gmail.com>
2024-01-14 15:59:29 +0100

    First deckbuilding scene rework

--- scripts/deckbuilding_phase/deckbuilding_scene.gd
+++ scripts/deckbuilding_phase/deckbuilding_scene.gd
@@ -1,24 +1,26 @@
 extends Node
 
-var _character_deck_editor_scene : PackedScene = preload("res://scenes/deckbuilding_phase/character_deck_editor.tscn")
-
 var host_presets : Array = []
 var client_presets : Array = []
 
-func _ready():
-	for i in range(3) :
-		$CharacterDeckEditors.get_child(i).character = GameManager.player.get_character(i)
 
+var selected_character_index : int :
+	set(value) : 
+		selected_character_index = value
+		$CharacterDisplay.character = selected_character
+		for i in range(3) : 
+			%PresetSelector.get_child(i).text = selected_character.deck_presets[i].preset_name
+		for i in range(5) : 
+			$CardArea.get_child(i).card = selected_character.card_pool[i]
+		update_card_counts()
 
-func _on_apply():
-	var presets : Array[int]
-	for editor in $CharacterDeckEditors.get_children() :
-		presets.append(editor.preset_index)
-	if NetworkManager.is_host : 
-		notify_preset_choice(presets)
-	else :
-		notify_preset_choice.rpc(presets)
+var selected_character : Character :
+	set(value) :
+		printerr("Cannot directly set character")
+	get :
+		return GameManager.player.get_character(selected_character_index)
 
+var selected_presets : Array[int] = [0, 0, 0]
 
 @rpc("any_peer", "call_remote", "reliable")
 func notify_preset_choice(presets : Array) -> void : 
@@ -42,3 +44,26 @@ func apply_presets(host_presets : Array, client_presets : Array) -> void :
 		client.get_character(char_index).use_preset(client_presets[char_index])
 		
 	GameManager.set_game_state(GameManager.GameState.COMBAT)
+
+func _on_apply() -> void:
+	if NetworkManager.is_host : 
+		notify_preset_choice(selected_presets)
+	else :
+		notify_preset_choice.rpc(selected_presets)
+
+func _ready():
+	selected_character_index = 0
+	for i in range(3) :
+		%PlayerCharacterSelector.get_child(i).text = GameManager.player.get_character(i).character_name
+		%OpponentCharacterSelector.get_child(i).text = GameManager.opponent.get_character(i).character_name
+
+func on_ally_character_selected(char_index : int) -> void :
+	selected_character_index = char_index
+
+func on_preset_selected(preset_index : int) -> void :
+	selected_presets[selected_character_index] = preset_index
+	update_card_counts()
+
+func update_card_counts() -> void :
+	for i in range(5) : 
+		%CardCounts.get_child(i).text = str(selected_character.deck_presets[selected_presets[selected_character_index]].content[i])
