874a880095b52b4a2e7956107659159d5b6aca1f
Nelirks <raphael.lesbros@gmail.com>
2024-01-20 16:49:25 +0100

    Setup Wwise project

--- GeneratedSoundBanks/wwise_ids.gd
+++ GeneratedSoundBanks/wwise_ids.gd
@@ -1,37 +1,86 @@
 class_name AK
 
 class EVENTS:
+
+	const CARD_DEBUFF = 3760597508
+	const START_MUSIC = 540993415
+	const START_MAIN_AMBIANCE = 1660258124
+	const BUTTON_HOVER = 1701177942
+	const CARD_BUFF = 2145643165
+	const DEFEAT = 1593864692
+	const CARD_DRAW = 4136169396
+	const END_TURN = 731380034
+	const TAKE_DAMAGE = 3020609900
+	const CARD_USE = 659043495
+	const BUTTON_CLICK = 814543256
+	const START_TURN = 3790030967
+	const VICTORY = 2716678721
+
 	const _dict = {
+		"Card_Debuff": CARD_DEBUFF,
+		"Start_Music": START_MUSIC,
+		"Start_Main_Ambiance": START_MAIN_AMBIANCE,
+		"Button_Hover": BUTTON_HOVER,
+		"Card_Buff": CARD_BUFF,
+		"Defeat": DEFEAT,
+		"Card_Draw": CARD_DRAW,
+		"End_Turn": END_TURN,
+		"Take_Damage": TAKE_DAMAGE,
+		"Card_Use": CARD_USE,
+		"Button_Click": BUTTON_CLICK,
+		"Start_Turn": START_TURN,
+		"Victory": VICTORY
 	}
 
 class STATES:
-	const _dict = {
-	}
+
+	const _dict = {}
 
 class SWITCHES:
-	const _dict = {
-	}
+
+	const _dict = {}
 
 class GAME_PARAMETERS:
-	const _dict = {
-	}
+
+	const _dict = {}
 
 class TRIGGERS:
-	const _dict = {
-	}
+
+	const _dict = {}
 
 class BANKS:
+
+	const INIT = 1355168291
+	const SOUNDBANK = 1661994096
+
 	const _dict = {
+		"Init": INIT,
+		"Soundbank": SOUNDBANK
 	}
 
 class BUSSES:
+
+	const MASTER_AUDIO_BUS = 3803692087
+
 	const _dict = {
+		"Master Audio Bus": MASTER_AUDIO_BUS
 	}
 
 class AUX_BUSSES:
-	const _dict = {
-	}
+
+	const _dict = {}
 
 class AUDIO_DEVICES:
+
+	const NO_OUTPUT = 2317455096
+	const SYSTEM = 3859886410
+
 	const _dict = {
+		"No_Output": NO_OUTPUT,
+		"System": SYSTEM
 	}
+
+class EXTERNAL_SOURCES:
+
+	const _dict = {}
+
