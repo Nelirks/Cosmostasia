1cc2709866b6b017540322fe853b950c25c14db4
Simon Gosse <simongosse.etude@gmail.com>
2024-01-21 19:36:47 +0100

    Player and opponent cards instanciation + scale fixing for cards

--- scripts/card_display/playable_card_2d.gd
+++ scripts/card_display/playable_card_2d.gd
@@ -13,3 +13,17 @@ func update_card_infos():
 	(%Card_Character as Label).text = card.character.character_name
 	(%CardDescription as RichTextLabel).text = card.description
 	(%CardFlavorText as RichTextLabel).text = card.quote
+	
+	match card.cardType:
+		card.Type.ATTACK:
+			%Attack_Border.visible = true
+			%Defense_Border.visible = false
+			%Strategy_Border.visible = false
+		card.Type.DEFENSE:
+			%Attack_Border.visible = false
+			%Defense_Border.visible = true
+			%Strategy_Border.visible = false
+		card.Type.STRATEGY:
+			%Attack_Border.visible = false
+			%Defense_Border.visible = false
+			%Strategy_Border.visible = true
