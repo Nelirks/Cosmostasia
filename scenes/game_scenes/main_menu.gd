9e9fe8a2bb2d1ed107783e57ab214c8cf3c20bf1
Simon Gosse <simongosse.etude@gmail.com>
2024-01-28 23:18:58 +0100

    Main menu visual setup + button change scene and scale on hover

--- scenes/game_scenes/main_menu.gd
+++ scenes/game_scenes/main_menu.gd
@@ -1,4 +1,12 @@
-extends Button
+extends Control
 
-func _on_pressed():
+func _on_texture_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/game_scenes/network_setup.tscn")
+
+
+func _on_texture_button_mouse_entered():
+	%TextureButton.scale = Vector2(1.1, 1.1)
+
+
+func _on_texture_button_mouse_exited():
+	%TextureButton.scale = Vector2(1, 1)
