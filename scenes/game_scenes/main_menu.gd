60ab79ac315311cb559532f06dcfefb2a063aefc
Simon Gosse <simongosse.etude@gmail.com>
2024-01-31 22:37:32 +0100

    replace play button by host and join buttons

--- scenes/game_scenes/main_menu.gd
+++ scenes/game_scenes/main_menu.gd
@@ -1,8 +1,10 @@
 extends Control
 
-func _on_texture_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/game_scenes/network_setup.tscn")
+func _on_host_button_pressed():
+	pass # Replace with function body.
 
+func _on_join_button_pressed():
+	pass # Replace with function body.
 
 func _on_texture_button_mouse_entered():
 	%TextureButton.scale = Vector2(1.1, 1.1)
@@ -10,3 +12,9 @@ func _on_texture_button_mouse_entered():
 
 func _on_texture_button_mouse_exited():
 	%TextureButton.scale = Vector2(1, 1)
+
+
+func _on_settings_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/game_scenes/settings_menu.tscn")
+
+
