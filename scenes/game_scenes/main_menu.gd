27027fa69aec5b9cab2f7b714f2d2254b7a20e3d
Simon Vacances <ruukoy@gmail.com>
2024-02-01 11:27:12 +0100

    added settings to main meu

--- scenes/game_scenes/main_menu.gd
+++ scenes/game_scenes/main_menu.gd
@@ -1,20 +1,13 @@
 extends Control
 
 func _on_host_button_pressed():
-	NetworkManager.create_server(( as TextEdit).text)
+	NetworkManager.create_server((%PortEdit as TextEdit).text)
 
 func _on_join_button_pressed():
-	NetworkManager.create_client(($IPEdit as TextEdit).text, ($PortEdit as TextEdit).text)
-
-func _on_texture_button_mouse_entered():
-	%TextureButton.scale = Vector2(1.1, 1.1)
-
-
-func _on_texture_button_mouse_exited():
-	%TextureButton.scale = Vector2(1, 1)
-
+	NetworkManager.create_client((%IPEdit as TextEdit).text, (%PortEdit as TextEdit).text)
 
 func _on_settings_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/game_scenes/settings_menu.tscn")
+	%SettingsBackground.set_position(Vector2(0,0))
 
-
+func _on_button_pressed():
+	%SettingsBackground.set_position(Vector2(-2000,0))
