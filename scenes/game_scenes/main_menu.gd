eb1c982e8addf0cab0f4a279a5adca7eb79bfe4e
Nelirks <raphael.lesbros@gmail.com>
2024-02-01 14:31:37 +0100

    Fixed game not starting

--- scenes/game_scenes/main_menu.gd
+++ scenes/game_scenes/main_menu.gd
@@ -1,5 +1,8 @@
 extends Control
 
+func _ready():
+	NetworkManager.connection_done.connect(_start_game)
+
 func _on_host_button_pressed():
 	NetworkManager.create_server((%PortEdit as TextEdit).text)
 
@@ -11,3 +14,6 @@ func _on_settings_button_pressed():
 
 func _on_button_pressed():
 	%SettingsBackground.set_position(Vector2(-2000,0))
+
+func _start_game() -> void :
+	GameManager.set_game_state(GameManager.GameState.DRAFT)
