a1fa9b50ff3e0c663e42562ac0bdc81c8487c8bc
Nelirks <raphael.lesbros@gmail.com>
2024-02-09 13:54:43 +0100

    1st balance patch (including Khemael and LR being able to play cards)

--- scripts/data/cards/khemael/ascension.gd
+++ scripts/data/cards/khemael/ascension.gd
@@ -1,21 +1,20 @@
 extends Card
 
-@export var damage : int
-@export var heal : int
-@export var stacks_threshold : int
+@export var damage_per_stack : int
+@export var heal_per_stack : int
 
 func apply_effects(target : Character) -> void :
+	if target.player != character.player : 
+		_add_effect(DamageEffect.new(damage_per_stack * character.get_status("khemael_passive").prejudice_stacks, character, target))
+	else : 
+		_add_effect(HealEffect.new(heal_per_stack * character.get_status("khemael_passive").faveur_stacks, character, target))
 	character.get_status("khemael_passive").faveur_stacks = 0
 	character.get_status("khemael_passive").prejudice_stacks = 0
-	if target.player != character.player : 
-		_add_effect(DamageEffect.new(damage, character, target))
-	else : 
-		_add_effect(HealEffect.new(heal, character, target))
 
 func can_target(target : Character) -> bool :
-	if character.has_status("khemael_passive") and character.get_status("khemael_passive").prejudice_stacks >= stacks_threshold :
+	if character.has_status("khemael_passive") and character.get_status("khemael_passive").prejudice_stacks > 0 :
 		return character.player != target.player
-	elif character.has_status("khemael_passive") and character.get_status("khemael_passive").faveur_stacks >= stacks_threshold :
+	elif character.has_status("khemael_passive") and character.get_status("khemael_passive").faveur_stacks > 0 :
 		return character.player == target.player
 	else : 
-		return false
+		return true
