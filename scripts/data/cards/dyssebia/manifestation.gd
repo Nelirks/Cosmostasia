393b82a00ace704bab21ef7f41cbc8db7bdda18c
Nelirks <raphael.lesbros@gmail.com>
2024-02-12 13:51:12 +0100

    Applied card balance, bug fixes, name changes and text changes

--- scripts/data/cards/dyssebia/manifestation.gd
+++ scripts/data/cards/dyssebia/manifestation.gd
@@ -5,5 +5,5 @@ extends Card
 var provoke_status : StackableStatusEffect = preload("res://resources/game_data/status_effect_data/provoke_status_effect.tres")
 
 func apply_effects(target : Character) -> void :
-	_add_effect(ApplyArmorEffect.new(armor, character, target))
-	_add_effect(ApplyStatusEffect.new(provoke_status.duplicate().set_stacks(provoke_stacks), character, target))
+	_add_effect(ApplyArmorEffect.new(armor, character, character))
+	_add_effect(ApplyStatusEffect.new(provoke_status.duplicate().set_stacks(provoke_stacks), character, character))
--- scripts/data/cards/khemael/suppression.gd
+++ scripts/data/cards/khemael/suppression.gd
@@ -3,5 +3,5 @@ extends Card
 @export var damage : int
 
 func apply_effects(target : Character) -> void :
-	_add_effect(RemoveStatusesEffect.new(false, true, character, target))
+	_add_effect(RemoveStatusesEffect.new(true, false, character, target))
 	_add_effect(DamageEffect.new(damage, character, target))
