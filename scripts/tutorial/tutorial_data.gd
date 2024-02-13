a066e87ee1c01e41fc02d06f81a265f8ccac0a15
electron35 <eleque.turone@outlook.com>
2024-02-13 14:49:21 +0100

    Make tutorial pretty

--- scripts/tutorial/tutorial_data.gd
+++ scripts/tutorial/tutorial_data.gd
@@ -2,5 +2,5 @@ extends Resource
 class_name tutorial_data
 
 @export var tutorial_name:String #X_name (where x is an integer)
-@export var text:String
+@export_multiline var text:String
 var used:bool = false
