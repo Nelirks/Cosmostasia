8f57affbbb90cbf4b6e2cc6d0e87505cafe87d4d
Bou <chloe.bigot11@gmail.vom>
2024-02-16 12:36:55 +0100

    Revert "Merge branch 'main' of https://github.com/Nelirks/Cosmostasia"
    This reverts commit 8cd1e37456bbd4fe1c32b749cad95c93f6b5908c, reversing
    changes made to 407b409094845d9f9ae75fc22285d1b40fe5d7af.

--- scripts/tutorial/tutorial_popup.gd
+++ scripts/tutorial/tutorial_popup.gd
@@ -1,13 +1,10 @@
 extends PanelContainer
 @onready var text_tutoriel = $TextTutoriel
 
-@export var text_data = ""
-
 func init(text:String):
-	text_data = text
 	$TextTutoriel.add_text(text)
 
 
-#func _on_button_pressed():
-#	TutorialGlobal.next()
-#	get_parent().queue_free()
+func _on_button_pressed():
+	TutorialGlobal.next()
+	get_parent().queue_free()
