5d3be09ad38c8060904aaf93d26d486a17dbbff0
Nelirks <raphael.lesbros@gmail.com>
2024-02-23 09:23:39 +0100

    Added Provoke to LR

--- scripts/data/cards/lord_reece/abandonne_felon.gd
+++ scripts/data/cards/lord_reece/abandonne_felon.gd
@@ -1,8 +1,6 @@
 extends Card
 
-@export var energy_regen : int
-
 func apply_effects(target : Character) -> void :
 	_add_effect(DiscardCardEffect.new([false, false, true], character, character.player.get_opponent()))
 	if position == Position.PREPARED_SLOT :
-		_add_effect(RegenEnergyEffect.new(energy_regen, true, character, character.player))
+		_add_effect(ApplyStatusEffect.new(preload("res://resources/game_data/status_effect_data/provoke_status_effect.tres").duplicate().set_stacks(1), character, character))
