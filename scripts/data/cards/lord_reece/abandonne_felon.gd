9dc03d36a073371a26e0a7a6f6419a56aa4a5ad6
Nelirks <raphael.lesbros@gmail.com>
2024-02-24 09:05:47 +0100

    Buffed Abandonne Felon

--- scripts/data/cards/lord_reece/abandonne_felon.gd
+++ scripts/data/cards/lord_reece/abandonne_felon.gd
@@ -1,6 +1,6 @@
 extends Card
 
 func apply_effects(target : Character) -> void :
-	_add_effect(DiscardCardEffect.new([false, false, true], character, character.player.get_opponent()))
+	_add_effect(ApplyStatusEffect.new(preload("res://resources/game_data/status_effect_data/provoke_status_effect.tres").duplicate().set_stacks(1), character, character))
 	if position == Position.PREPARED_SLOT :
-		_add_effect(ApplyStatusEffect.new(preload("res://resources/game_data/status_effect_data/provoke_status_effect.tres").duplicate().set_stacks(1), character, character))
+		_add_effect(DiscardCardEffect.new([false, false, true], character, character.player.get_opponent()))
